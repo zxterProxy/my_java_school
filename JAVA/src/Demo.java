2379f5ac71c21ff07264d41f1e4173a70f9deaf1
zxterProxy <47256412+zxterProxy@users.noreply.github.com>
2021-07-10 13:42:12 +0300

    первые шаги

---
+++ JAVA/src/Demo.java
@@ -0,0 +1,14 @@
+/**
+ * Created by zxter on 10.07.2021.
+ */
+import static javax.swing.JOptionPane.showMessageDialog;
+public class Demo {
+    public static void main(String[] args) {
+        /*
+        * Здарова
+        *  
+        * */
+        System.out.println("Привет");
+        showMessageDialog(null,"Ariamis");
+    }
+}
